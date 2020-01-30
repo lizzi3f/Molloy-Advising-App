27d0cb09d33cdcae9fdbe1f9d6478bed00b8958e
RichDoherty <doherty622@gmail.com>
2020-01-30 16:10:00 -0500

    1/30/20 dummy data

--- imports/collections/courses.js
+++ imports/collections/courses.js
@@ -1,3 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 
-export const Courses = new Mongo.Collection('courses');
+// const uri = "mongodb+srv://rdoherty@lions.molloy.edu:10547238@advisingmolloydb-b0iqm.mongodb.net/test?retryWrites=true&w=majority";
+
+ export const Courses = new Mongo.Collection('courses');
