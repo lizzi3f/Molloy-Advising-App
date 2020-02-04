882c7eb5251c1084798c2d567d163589db23b939
lizzi3f <43049640+lizzi3f@users.noreply.github.com>
2020-02-04 01:05:07 -0500

    Add files via upload

--- client/components/header.js
+++ client/components/header.js
@@ -8,18 +8,9 @@ import '../CSS/logout';
 const Header = () => {
   return (
     <header>
-      <nav>
-        <p>Molloy Advising</p>
-        <div className="container">
-                  <div id="menu-icon"><img alt="menu" src="/images/menuicon.png" width="40px" height="40px" /></div>
-                  <div className="dropdown-content">
-                      <a href= "main.html">Home</a>
-                      <a href= "coursesearch.html">Course Search</a>
-                      <a href= "myacademics.html">My Academics</a>
-                      <a href= "logout.html" className="logout">Logout</a>
-                  </div>
-      </div>
-    </nav>
+        <nav>
+            <p>Molloy Advising</p>
+        </nav>
     </header>
   );
 };
