17067adb290cdc0673062b2d6bb34ed9471e6941
lizzi3f <43049640+lizzi3f@users.noreply.github.com>
2020-02-04 01:03:22 -0500

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './components/header';
 import Footer from './components/footer';
-import CourseSearch from './components/course_search';
-import CourseResults from './components/course_results';
+import Home from './components/main_home';
+import MainCourseSearch from './components/main_cs';
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <CourseSearch />
-      <CourseResults />
-      <Footer />
-    </div>
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/MainCourseSearch">Course Search</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/MainCourseSearch">
+                        <MainCourseSearch />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
   );
 };
 
